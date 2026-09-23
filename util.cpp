87e5fa00b4b23a01df530345fe5620e505d835e0
Katarina Avlijas <katarina.avlijas@gmail.com>
2026-09-22 17:17:01 -0700

    Completed parseStringToWords

--- util.cpp
+++ util.cpp
@@ -15,15 +15,29 @@ std::string convToLower(std::string src)
     to a set of words based on the criteria given in the assignment **/
 std::set<std::string> parseStringToWords(string rawWords)
 {
+    set<string> keyWords; //empty set that can hold strings *in this case the keywords
+    string currentWord; //creates empty string to hold the current word being checked
 
+    for(size_t i = 0; i < rawWords.size(); i++){
+        char letter = rawWords[i];
 
+        if(isalnum(letter)){ //checks if the current char is a letter or digit
+            currentWord += letter;
+        }
+        else{
+            if(currentWord.size() >= 2){ //length check
+                keyWords.insert(convToLower(currentWord));//save the word
+            }
+            currentWord = ""; //reset current word to empty
+        }
+    }
 
+    //checks the current word and saves it if valid to make sure all keywords are saved 
+    if(currentWord.size() >= 2){ //length check
+                keyWords.insert(convToLower(currentWord));//save the word
+    }
 
-
-
-
-
-
+    return keyWords; //return the set of keywords
 
 }
 
